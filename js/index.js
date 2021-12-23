3da4a818c17cb95a315121cf2f72b22d994077a7
Tasin Shafi Leon <leontasinshafi@gmail.com>
2021-12-23 19:13:55 +0600

    added newsletter signup in portfolio

--- js/index.js
+++ js/index.js
@@ -241,3 +241,16 @@
   });
 
 })()
+
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+document.getElementById("ageSpan").innerHTML = getAge("1997/04/19");
